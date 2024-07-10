ea4781cdedf557c4bc4fe82b8d4a9dc843cbfd65
k-ghale <kabinghale888@gmail.com>
2024-07-10 22:47:02 +0545

    flip a coin

---
+++ flip-a-coin/flip/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FlipCoin from './components/FlipCoin'
+
+function App() {
+  return (
+    <div className='App'>
+      <FlipCoin />
+    </div>
+  );
+}
+
+export default App;
